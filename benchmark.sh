aaf5c53cdb49419cc011fbf574d36782dbf29d38
Karsten Silz <ksilz@outlook.com>
2025-06-30 19:54:21 +0200

    More runs.

--- benchmark.sh
+++ benchmark.sh
@@ -149,8 +149,8 @@ else
   TRAIN_CMD="java -XX:+UseG1GC ${AOT_FLAG} -jar $JAR_PATH --spring.profiles.active=postgres"
 fi
 CSV_FILE="result_${LABEL}.csv"
-WARMUPS=1
-RUNS=4
+WARMUPS=3
+RUNS=7
 
 # Debug information for configuration
 echo "DEBUG: Configuration set:"
